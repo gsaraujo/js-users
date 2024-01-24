8a6b32a9c081e8c699ce9208a7587f768f40ae7d
Gustavo <sasaeucomvoce@hotmail.com>
2024-01-23 21:08:12 -0300

    feat: creating User model for MVC pattern

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ var user = {};
 
 function addLine(dataUser) {
 
-    var tr = document.createElement("tr");
+    console.log(dataUser);
 
-    tr.innerHTML = `<tr>
+    document.getElementById("table-users").innerHTML =
+                    `<tr>
                         <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
                         <td>${dataUser.name}</td>
                         <td>${dataUser.email}</td>
@@ -17,10 +18,6 @@ function addLine(dataUser) {
                         <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
                         </td>
                     </tr>` ;
-
-
-    document.getElementById("table-users").appendChild(tr);
-
 
 }
 
@@ -42,7 +39,18 @@ document.getElementById("form-user-create").addEventListener("submit", function(
 
     });
 
-    addLine(user);
+    var objectUser = new User(
+        user.name,
+        user.gender,
+        user.birth,
+        user.country,
+        user.email,
+        user.password,
+        user.photo,
+        user.admin
+    );
+
+    addLine(objectUser);
 
 
 });
---
+++ models/User.js
@@ -0,0 +1,20 @@
+class User {
+
+    constructor(name, gender, birthdate, country, email, password, photo, admin) {
+
+        this.name = name;
+        this.gender = gender;
+        this.birthdate = birthdate;
+        this.country = country;
+        this.email = email;
+        this.password = password;
+        this.photo = photo;
+        this.admin = admin;
+
+
+    }
+
+
+
+
+}
