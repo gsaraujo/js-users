bb1fcb9df18b084c66e2364c24e145434f500102
Gustavo <sasaeucomvoce@hotmail.com>
2024-01-30 16:28:28 -0300

    feat: disable submit button and clearing the form

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -17,6 +17,10 @@ class UserController {
 
             event.preventDefault(); //prevent submit on the form
 
+            let btn = this.formEl.querySelector("[type=submit]");
+
+            btn.disabled = true;
+
             let values = this.getValues();
 
             //values.photo = "";
@@ -26,6 +30,10 @@ class UserController {
                 values.photo = content;
 
                 this.addLine(values);
+
+                this.formEl.reset();
+
+                btn.disabled = false;
 
 
             }, (e)=>{
